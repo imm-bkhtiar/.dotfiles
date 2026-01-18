fd33776c42a1e12a90d30b56bbaf29e92afa780b
imm-bkhtiar <156558879+imm-bkhtiar@users.noreply.github.com>
2026-01-18 16:50:23 +0700

    update

--- personal/mouse-clicky/mouseClicky.sh
+++ personal/mouse-clicky/mouseClicky.sh
@@ -3,8 +3,14 @@
 DEVICE_ID=14
 SOUND="./mouseClickSound.mp3"
 
-xinput test "$DEVICE_ID" | while read -r line; do
-    if echo "$line" | grep -qE "button press\s+(1|3)"; then
-        pw-play "$SOUND" &
-    fi
+# xinput test "$DEVICE_ID" | while read -r line; do
+#     if echo "$line" | grep -qE "button press\s+(1|3)"; then
+#         pw-play "$SOUND" &
+#     fi
+# done
+
+xinput test "$DEVICE_ID" \
+| grep --line-buffered "button press   1\|button press   3" \
+| while read -r _; do
+    pw-play "$SOUND" &
 done
--- run_this.sh
+++ run_this.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 ln -sf $HOME/.dotfiles/config/default/* $HOME/.config/
+
+# printf "The compositor ? \n1. X11 \n2. Wayland \n : "
+# read compositor
+#
+# printf "do you want add config to super user ? (y/n) : "
+# read addsu
+#
+# if [[ $compositor == 1 ]]; then
+#   ln -sf $HOME/.dotfiles/config/default/* $HOME/.config/
+# elif [[ $compositor == 2 ]]; then
+#   ln -sf $HOME/.dotfiles/config/additional/
+# fi
