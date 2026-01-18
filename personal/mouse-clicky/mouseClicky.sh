3e40e843876aaeaadea10f7e856efce454d9cb59
imm-bkhtiar <156558879+imm-bkhtiar@users.noreply.github.com>
2026-01-18 20:22:59 +0700

    update

--- personal/mouse-clicky/mouseClicky.sh
+++ personal/mouse-clicky/mouseClicky.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-DEVICE_ID=14
+get_mouse_id() {
+    xinput list |
+    grep -i "slave  pointer" |
+    grep -Ei "mouse|usb|wireless|logitech|razer|steelseries" |
+    sed -n 's/.*id=\([0-9]\+\).*/\1/p' |
+    head -n1
+}
+
+DEVICE_ID=$(get_mouse_id)
+
+if [[ $DEVICE_ID == "" ]]; then
+    DEVICE_ID=$(xinput list |
+        grep -i "touchpad" |
+        sed -n 's/.*id=\([0-9]\+\).*/\1/p')
+fi
+
 SOUND="./mouseClickSound.mp3"
 
 # xinput test "$DEVICE_ID" | while read -r line; do
