15ac938c36c1b1374a37c3ab71a82747ecc0d44a
imm-bkhtiar <156558879+imm-bkhtiar@users.noreply.github.com>
2026-02-19 15:36:36 +0700

    update

---
+++ config/default/rofi/modules/noteMenu/noteMenu.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+rodir="$HOME/BAKHTIAR/note"
+selected=$(find $rodir -type f | \
+  while read -r full; do 
+    name=$(basename "$full")
+    printf "$name\x00icon\x1f%s\n" "$HOME/.config/rofi/modules/noteMenu/img/note.svg"
+  done | \
+  rofi -dmenu -i -p "My Note" -theme $HOME/.config/rofi/config_flat.rasi)
+
+if [[ $selected != "" ]]; then
+  kitty --hold nvim "$rodir/$selected"
+else
+  exit
+fi
+
