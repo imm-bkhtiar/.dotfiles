0e8a63f05affcf9a17740dd97653f8680420a33f
imm-bkhtiar <156558879+imm-bkhtiar@users.noreply.github.com>
2026-02-20 22:33:14 +0700

    update

--- config/default/i3/polybar.sh
+++ config/default/i3/polybar.sh
@@ -4,9 +4,13 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do
+  echo true
+  sleep 1; 
+done
 
 # Launch polybar
 polybar &
 sleep 0.2
+$HOME/.config/i3/polybarDefault.sh
 polybar-msg action "#powerBtn.module_hide"
---
+++ config/default/i3/polybarSwitch.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+polybar-msg action "#xwindow.module_toggle"
+polybar-msg action "#wlan.module_toggle"
+polybar-msg action "#cpu.module_toggle"
+polybar-msg action "#memory.module_toggle"
+polybar-msg action "#battery.module_toggle"
+polybar-msg action "#backlight.module_toggle"
+polybar-msg action "#pulseaudio.module_toggle"
+polybar-msg action "#mpd.module_toggle"
+polybar-msg action "#mpd2.module_toggle"
+polybar-msg action "#date.module_toggle" 
