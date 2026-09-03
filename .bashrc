66a47db02bc76d393fa37bdb3949d920bce7b18f
imm-bkhtiar <156558879+imm-bkhtiar@users.noreply.github.com>
2026-09-03 22:49:51 +0700

    update

--- .bashrc
+++ .bashrc
@@ -119,3 +119,4 @@ export PATH="$PATH:~/.local/kitty.app/bin"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+. "$HOME/.cargo/env"
---
+++ personal/rofi-menu/rofi-menu.sh
@@ -0,0 +1,33 @@
+#!/bin/env bash
+
+rodir="/mnt/windows/Videos/$1"
+
+if [[ $1 == "Music" ]]; then
+  rodir="/mnt/windows/Music"
+fi
+
+if [[ $1 == "Anime" || $1 == "Series" || $1 == "Music" ]]; then 
+  mapfile -t files < <(find "$rodir" -mindepth 1 -maxdepth 1 \( -type d -o -type f \) ! \( -name ".*" -o -name "*.rar" \) | sort)
+else
+  mapfile -t files < <(find "$rodir" -type f ! -name ".*" | sort)
+fi
+
+selected=$(
+  printf '%s\n' "${files[@]}" |
+    while read -r file; do
+      printf "%s\0icon\x1f$1\n" "$(basename "$file")"
+    done |
+      rofi -dmenu -i -p "mpv" -theme $HOME/.config/rofi/config_flat.rasi
+)
+
+if [[ $selected != "" && $1 != "Music" ]]; then
+  mpv "$rodir/$selected"
+elif [[ $selected != "" && $1 == "Music" ]]; then
+  systemctl --user start mpd
+  mpc update
+  mpc clear
+  mpc add "$selected"
+  mpc play
+else
+  exit
+fi
